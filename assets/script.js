a960677d83b2ed2cd3541e55bb0c2e2fcfa7052f
Alex Hsieh <alexhsieh12@gmail.com>
2022-09-20 11:53:41 -0700

    push

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,6 @@
 var localStorageSaves = new Array();
 var save = $('.storage')
 
-
-
-
 var moment = moment().format("ddd, MMM YYYY "); 
 $('#1A').text(moment);
 
@@ -13,11 +10,26 @@ save.on('click', function change(event) {
         var userInput = $(this).siblings('.middleSection').children('.controller').val()
         console.log(userInput)
         localStorage.setItem(hourKey, userInput)
-        save.classList.remove('hide')
+        $('.reminder').removeClass('hide')
 })
 
 $('#9A').val(localStorage.getItem(9))
 
 for(var i = 9;i <18; i++) {
     $(`#${i}A`).val(localStorage.getItem(i))
+}
+
+for(var p = 9; p < 18; p++) {
+    var currentTime = {value: "time", timestamp: new Date().getTime()}
+    localStorage.setItem("time", JSON.stringify(currentTime));
+    
+    var displayTime = JSON.parse(currentTime)
+    console.log(currentTime)
+    if (p < currentTime) {
+        $('controller').addClass(past)
+    } if (p == currentTime) {
+        $('controller').addClass(present)
+    } else {
+        $('controller').addClass(future)
+    }
 }
