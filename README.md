8450fad22fa6c13d39f6e5b72247a6d967f9a83d
Alex Hsieh <alexhsieh12@gmail.com>
2022-09-20 21:43:05 -0700

    created final draft with readme file incorporated

---
+++ README.md
@@ -0,0 +1,15 @@
+# <Daily Calendar>
+
+## Description
+My motivation was to design a calendar that would allow me to document what I have to do throughout the day and keep track of what events and times have already passed. It helped me learn a lot about javascript and how to utilize more for loops. I was also able to learn from the code that I had written and understand and dive into where some of the issues were at. It solves the problem of having just a simple planner with no information on it. Compared to mine where it will change colors based on what times have passed.
+
+## Installation
+
+All you need to do is open the github pages application and it will display my calendar!
+
+## Usage
+For this Calendar website, I had to utilize many elements such as jquery and moment.js in order to incorporate features such as formatting and time. I created my code with simplicity in mind in order to make it easy to read while completing the tasks needed by the user. It now successfully changes colors based on the time(red in the past, grey for the present, and green for the future). It also stores whatever is written in the time slots to an ID card and puts it into local storage to be further called upon. 
+
+
+![Picture of my application] (./assets/images/deployedApplication.png)
+![link to the deployed application] (https://github.com/FigWheyMutton/Calendar)
--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
+
 var localStorageSaves = new Array();
 var save = $('.storage')
 
 var moment = moment().format("ddd, MMM YYYY "); 
-$('#1A').text(moment);
 
 save.on('click', function change(event) {
         event.preventDefault()
@@ -13,23 +13,20 @@ save.on('click', function change(event) {
         $('.reminder').removeClass('hide')
 })
 
-$('#9A').val(localStorage.getItem(9))
-
-for(var i = 9;i <18; i++) {
-    $(`#${i}A`).val(localStorage.getItem(i))
-}
-
-for(var p = 9; p < 18; p++) {
-    var currentTime = {value: "time", timestamp: new Date().getTime()}
-    localStorage.setItem("time", JSON.stringify(currentTime));
-    
-    var displayTime = JSON.parse(currentTime)
-    console.log(currentTime)
-    if (p < currentTime) {
-        $('controller').addClass(past)
-    } if (p == currentTime) {
-        $('controller').addClass(present)
-    } else {
-        $('controller').addClass(future)
-    }
+let currentTime = moment();
+let elements = document.querySelectorAll(".time")
+let time = 9;
+for (element of elements) {
+  let setTime = moment().hours(time).minutes(0).seconds(0);
+  element.dataset.time = moment(setTime).unix();
+  if (currentTime.unix() > setTime.unix()) {
+    element.classList.add("past")
+  }
+  if (currentTime.unix() < setTime.unix()) {
+    element.classList.add("future")
+  }
+  if (moment(currentTime).hour() == moment(setTime).hour()) {    
+    element.classList.add("present")
+  }
+  time++
 }
