97e506907dcc62f622812795ba16ec0596789d8a
Alex Hsieh <alexhsieh12@gmail.com>
2022-09-24 12:10:04 -0700

    commiit

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,38 @@
+// var localStorageSaves = new Array();
+// var save = $('.storage')
 
+// var moment = moment().format("ddd, MMM YYYY "); 
+// $('#1A').text(moment);
+
+// save.on('click', function change(event) {
+//         event.preventDefault()
+//         var hourKey = $(this).attr("id");  
+//         var userInput = $(this).siblings('.middleSection').children('.controller').val()
+//         console.log(userInput)
+//         localStorage.setItem(hourKey, userInput)
+//         $('.reminder').removeClass('hide')
+// })
+
+// $('#9A').val(localStorage.getItem(9))
+
+// for(var i = 9;i <18; i++) {
+//     $(`#${i}A`).val(localStorage.getItem(i))
+// }
+
+// for(var p = 9; p < 18; p++) {
+//     var currentTime = {value: "time", timestamp: new Date().getTime()}
+//     localStorage.setItem("time", JSON.stringify(currentTime));
+    
+//     var displayTime = JSON.parse(currentTime.timestamp)
+//     console.log(currentTime)
+//     if (p < currentTime) {
+//         $('controller').addClass('past')
+//     } if (p == currentTime) {
+//         $('controller').addClass('present')
+//     } else {
+//         $('controller').addClass('future')
+//     }
+// }
 var localStorageSaves = new Array();
 var save = $('.storage')
 
